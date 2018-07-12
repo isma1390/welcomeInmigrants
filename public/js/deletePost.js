183f92fff0b22c6b24f439f7370ea84b36e21d2b
PaulaAraya <paula.arayawilliams@gmail.com>
2018-07-12 16:11:31 -0300

    sincronizar

--- public/js/deletePost.js
+++ public/js/deletePost.js
@@ -1,10 +1,10 @@
 function deletePost() {
     const deletePost = document.getElementById("deletePost")
     deletePost.addEventListener("click", ()=>{
-        db.collection("post").doc("DC").delete().then(function() {
-            console.log("Document successfully deleted!");
+        db.collection('post').doc('post.id').delete().then(function() {
+            console.log("El post ha sido borrado");
         }).catch(function(error) {
-            console.error("Error removing document: ", error);
+            console.error("Error removiendo el documento ", error);
         });
     })
 }
