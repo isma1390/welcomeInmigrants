05b5396450c989bd79680b6422b68475190561e1
PaulaAraya <paula.arayawilliams@gmail.com>
2018-07-15 17:19:58 -0300

    funcion eliminar

--- public/js/post.js
+++ public/js/post.js
@@ -1,48 +1,31 @@
-
-function posting() {
-  function toggleMenu() {
-    // añadir función onclick="toggleMenu()" al botón del nav bar y al botón cerrar.
-    if (sideMenu.className.indexOf("menu_closed") >= 0) {
-      // primero revisamos si la clase d-none esta
-      openMenu(); // si esta la clase quiere decir que el menú esta cerrado, asi que llamamos la funcion para abrirlo
-    } else {
-      closeMenu(); //si no esta la clase, le indicamos que cierre el menu
-    }
-  }
-
-  function openMenu() {
-    sideMenu.classList.remove("menu_closed"); // quitando clase display-none
-    sideMenu.classList.add("menu_open");
-  }
-
-  function closeMenu() {
-    sideMenu.classList.add("menu_closed"); // añadimos la clase display-none
-    sideMenu.classList.remove("menu_open");
-  }
-
   // POST USUARIO
-  const sendPost = document.getElementById("sendPost");
-  sendPost.addEventListener("click", () => {
+  function posting() {
+    const sendPost = document.getElementById("sendPost");
+    sendPost.addEventListener("click", () => {
     firebase
       .database()
       .ref("post")
       .limitToLast(5)
-      .on("child_added", newMessage => {
+      .on("child_added", (newPost) => {
         postPrint.innerHTML += `
-        <ul class="list-group list-group-flush" style="width: 100%;" id= "${newMessage.Key}" >
+        <ul class="list-group list-group-flush" style="width: 100%;" id= "${newPost.Key}" >
                   <li class="list-group-item">
-                  <h6 class="card-title">${newMessage.val().creator}</h6>
-                  <p class="card-text text-justify">${newMessage.val().text}</p>
-                  <i class="fas fa-edit"> | </i> <i id="DeletePost" class="fas fa-trash-alt" data-idPost = "${newMessage.Key}" > | </i> 
+                  <h6 class="card-title">${newPost.val().creator}</h6>
+                  <p class="card-text text-justify">${newPost.val().text}</p>
+                  <i class="fas fa-edit"> | </i> <i class="fas fa-trash-alt" id="deletePost" oneclick="deletePosting (event)" data-post = "${newPost.Key}" ></i> 
                   </li>
                 </ul>
             `;
-      });
+      });  
+    });
+  };
 
+
+  function sendPosting() {
     const currentUser = firebase.auth().currentUser;
-    const messageAreaText = messageArea.value;
-    //Para tener una nueva llave en la colección messages
-    let newMessageKey = firebase
+    const postAreaText = postArea.value;
+    //Para tener una nueva llave en la colección Posts
+    let newPostKey = firebase
       .database()
       .ref()
       .child("post")
@@ -50,23 +33,32 @@ function posting() {
 
     firebase
       .database()
-      .ref(`post/${newMessageKey}`)
+      .ref(`post/${newPostKey}`)
       .set({
         creator: currentUser.displayName,
         creatorName: currentUser.email,
-        text: messageAreaText
+        text: postAreaText
       });
-  });
-}
+  }
 
-//función para borrar post
-function deletePost() {
+  //borrar post
+    function deletePosting (event) {
+      event.stopPopagation();
+      const postId = event.target.getAttribute("data-post");
+      const postRef = firebase.database().ref("post").child(postId);
+      postRef.remove();
+      postPrint.removeChild(postPrint.childNodes[0] && postPrint.childNodes[1]);
+    }
+/*
+//borrar post
+
 const deletePost = document.getElementById("deletePost")
-deletePost.addEventListener('click',(p) => {
-  p.stopPropagation();
-  const idPost = p.target.getAttribute("data-idPost")
-  const postRef = dbRef.child('post/' + idPost);
-  postRef.remove()
- })
-};
+deletePost.addEventListener('click',() => {
+  e.stopPropagation();
+  const idPost = e.target.getAttribute("data-idPost")
+  const postRef = firebase.database().ref('post/' + idPost);
+  postRef.remove();
 
+ });
+
+*/
