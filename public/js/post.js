4485dbfb9cfc0c6bf11afc65b1fa190a4b4839a7
PaulaAraya <paula.arayawilliams@gmail.com>
2018-07-16 22:00:32 -0300

    cambios en evento click de post

--- public/js/post.js
+++ public/js/post.js
@@ -1,23 +1,9 @@
   // POST USUARIO
 function posting() {
   sendPost.addEventListener("click", () => {
-    firebase
-      .database()
-      .ref("post")
-      .limitToLast(5)
-      .on("child_added", newPost => {
-        postPrint.innerHTML += `
-                <ul class="list-group list-group-flush" style="width: 100%;" id="${newPost.Key}"> 
-                  <li class="list-group-item">
-                  <h6 class="card-title">${newPost.val().creator}</h6>
-                  <p class="card-text text-justify">${newPost.val().text}</p>
-                  <i class="fas fa-edit" onclick="updatePost()"> </i> <i class="fas fa-trash-alt" data-post="${newPost.Key}" onclick="deletePost(event)"></i> 
-                  </li>
-                </ul>
-            `;
-      });
       const currentUser = firebase.auth().currentUser;
       const postAreaText = postArea.value;
+      postArea.value = " ";
       //Para tener una nueva llave en la colección messages
       let newPostKey = firebase
         .database()
@@ -30,34 +16,51 @@ function posting() {
         .ref(`post/${newPostKey}`)
         .set({
           creator: currentUser.displayName,
-          creatorName: currentUser.email,
+          creatorEmail: currentUser.email,
           text: postAreaText
         });  
   });
 }
   // Borrar post
 function deletePost(event) {
-  event.stopPropagation();
+  event.stopPropagation(); //se activa solamente donde se hace click
   const postId = event.target.getAttribute("data-post");
   const postRef = firebase.database().ref("post").child(postId);
-  postRef.remove();
-  postPrint.removeChild(postPrint.childNodes[0] && postPrint.childNodes[1]);  
+  postRef.remove()
+  .then(function() {
+    console.log("El documento ha sido borrado");
+  })
+  .catch(function(error) {
+    // The document probably doesn't exist.
+    console.error("Error al borrar el documento ", error);
+  });
 }
 
 // Editar post
-function updatePost() {
-  let postEditRef = firebase.database().ref(`post/${newPostKey}`);
+function updatePost(eventUpdate) {
+  const postText = eventUpdate.target.getAttribute("data-post");
+  const TextRef = firebase.database().ref("post").child(postText);
+  TextRef.update()  
+    .then(function() {
+      console.log("El documento ha sido editado");
+    })
+    .catch(function(error) {
+    // The document probably doesn't exist.
+      console.error("Error al editar el documento ", error);
+    });
+}
 
-  // 
-  return postEditRef.update({
-  text: messageAreaText
-  })
-  .then(function() {
-  console.log("El documento ha sido editado");
-  })
-  .catch(function(error) {
-  // The document probably doesn't exist.
-  console.error("Error al editar el documento ", error);
-});
-  
+const drawPosts = (posts) => {
+  console.log(posts);
+  Object.entries(posts.val()).forEach((post) => { 
+    postPrint.innerHTML += `
+            <ul class="list-group list-group-flush" style="width: 100%;"> 
+              <li class="list-group-item">
+              <h6 class="card-title">${post[1].creator}</h6>
+              <p class="card-text text-justify">${post[1].text}</p>
+              <i class="fas fa-edit" data-text="${post[1].text}" onclick="updatePost("eventUpdate") id="editPost"> </i> <i class="fas fa-trash-alt" data-post="${post[0].Key}" onclick="deletePost(event)"></i> 
+              </li>
+            </ul>
+        `;
+  });
 }
